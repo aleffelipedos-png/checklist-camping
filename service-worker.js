9e7eeae92799e605a93f38ead9eb65a8ae40dc5d
aleffelipedos-png <aleffelipedos@gmail.com>
2026-09-12 13:50:50 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='meu-camping-v7522';
+const CACHE='meu-camping-v754';
 const CORE=['./','./index.html','./manifest.json','./camping-banner.jpg','./avatar-1.webp','./avatar-2.webp','./avatar-3.webp','./avatar-4.webp','./avatar-5.webp','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
